2815c3028d4d1d6cca62a831a6509e364655fc2b
Aaron Blakely <aaron@ephasic.org>
2016-02-14 13:52:16 -0500

    updates

--- modules/ChanOP.pm
+++ modules/ChanOP.pm
@@ -6,15 +6,15 @@ my $help = Shadow::Help;
 sub loader {
   $bot->add_handler('chancmd op',    'chanop_op');
   $bot->add_handler('privcmd op',    'chanop_op');
-  $bot->add_handler('chancmd deop',  'chanop_deop');
-  $bot->add_handler('privcmd deop',  'chanop_deop');
+  $bot->add_handler('chancmd voice', 'chanop_voice');
+  $bot->add_handler('privcmd voice', 'chanop_voice');
   $bot->add_handler('privcmd kick',  'chanop_kick');
   $bot->add_handler('chancmd kick',  'chanop_kick');
   $bot->add_handler('chancmd ban',   'chanop_ban');
   $bot->add_handler('privcmd ban',   'chanop_ban');
 
-  $help->add_help('op', 'Channel', '<user> <channel>', 'Give operator mode to a user. [F]', 0);
-  $help->add_help('deop', 'Channel', '<user> <channel>', 'Removes operator mode from a user. [F]', 0);
+  $help->add_help('op', 'Channel', '<user> <channel>', 'Give or removes operator mode to a user. [F]', 0);
+  $help->add_help('voice', 'Channel', '<user> <channel>', 'Gives or removes voice from a user. [F]', 0);
   $help->add_help('kick', 'Channel', '<user> <channel> <reason>', 'Kicks a user from a channel. [F]', 0);
   $help->add_help('ban', 'Channel', '<user> <channel> <reason>', 'Bans a user from a channel. [F]', 0);
 }
@@ -72,11 +72,15 @@ sub chanop_op {
     my ($n, $c) = split(/ /, $text);
     $c = $chan if !$c;
 
-    $bot->op($c, $n);
+    if ($bot->isop($n, $c)) {
+    	$bot->deop($c, $n);
+    } else {
+        $bot->op($c, $n);
+    }
   }
 }
 
-sub chanop_deop {
+sub chanop_voice {
   my ($nick, $host, $chan, $text) = @_;
   if (!$text) {
     $text = $chan;
@@ -86,8 +90,28 @@ sub chanop_deop {
     my ($n, $c) = split(/ /, $text);
     $c = $chan if !$c;
 
-    $bot->deop($c, $n);
+    if ($bot->isvoice($n, $c)) {
+	$bot->devoice($c, $n);
+    } else {
+	$bot->voice($c, $n);
+    }
   }
-}
+} 
 
+sub unloader {
+  $bot->del_handler('chancmd op',    'chanop_op');
+  $bot->del_handler('privcmd op',    'chanop_op');
+  $bot->del_handler('chancmd voice', 'chanop_voice');
+  $bot->del_handler('privcmd voice', 'chanop_voice');
+  $bot->del_handler('privcmd kick',  'chanop_kick');
+  $bot->del_handler('chancmd kick',  'chanop_kick');
+  $bot->del_handler('chancmd ban',   'chanop_ban');
+  $bot->del_handler('privcmd ban',   'chanop_ban');
+
+
+  $help->del_help('op', 'Channel');
+  $help->del_help('voice', 'Channel');
+  $help->del_help('kick', 'Channel');
+  $help->del_help('ban', 'Channel');
+}
 1;
