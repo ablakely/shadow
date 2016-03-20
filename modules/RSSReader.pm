70a8451d04185fa83696298e89ad616b87774abb
Aaron Blakely <aaron@ephasic.org>
2016-03-20 11:16:23 -0500

    updates to RSS module

--- modules/RSSReader.pm
+++ modules/RSSReader.pm
@@ -17,6 +17,7 @@ sub loader {
   $bot->add_handler('privcmd delfeed', 'RSSReader_delfeed');
   $bot->add_handler('chancmd rsssync', 'RSSReader_rsssync');
   $bot->add_handler('privcmd listfeeds', 'RSSReader_listfeed');
+  $bot->add_handler('privcmd rssdbcleanup', 'RSSReader_dbcleanup');
 
   $help->add_help('addfeed', 'RSSReader', '<chan> <title> <url>', 'Enables the RSS Reader module.');
   $help->add_help('delfeed', 'RSSReader', '<chan> <title>', 'Deletes a RSS feed.');
@@ -100,10 +101,14 @@ sub RSSReader_genfeed {
   $ua->get("$feed" => json => {a => 'b'} => sub {
     my ($ua, $tx) = @_;
 
+    if (my $err = $tx->error) {
+      return $bot->say($chan, "RSS error on feed [$title]: ".$err);
+    }
+
     my $body = \scalar($tx->res->body);
 
     if (!$feedcache{$feed}) {
-      $f = $feedcache{$feed} = XML::Feed->parse($body) or return $bot->say($chan, "RSS error on feed [$title]: ".XML::Feed->errstr);
+      $f = $feedcache{$feed} = XML::Feed->parse($body) or return $bot->say($chan, "RSS parser error on feed [$title]: ".XML::Feed->errstr);
     } else {
       $f = $feedcache{$feed};
     }
@@ -134,6 +139,24 @@ sub RSSReader_feedagrigator {
   }
 
   $bot->add_timeout($SYNCTIME, 'RSSReader_feedagrigator');
+}
+
+sub RSSReader_dbcleanup {
+  my ($nick, $host, $text) = @_;
+
+  if ($bot->isbotadmin($nick, $host)) {
+    $bot->notice($nick, "Preforming RSS Feed database cleanup.");
+
+    my $db = RSSReader_readdb();
+
+    foreach my $chan (keys $db) {
+      foreach my $title (keys $db->{$chan}) {
+        $db->{$chan}->{$title}->{readposts} = [];
+      }
+    }
+
+    RSSReader_writedb($db);
+  }
 }
 
 sub RSSReader_addfeed {
@@ -184,6 +207,8 @@ sub unloader {
   $bot->del_handler('privcmd delfeed', 'RSSReader_delfeed');
   $bot->del_handler('chancmd rsssync', 'RSSReader_rsssync');
   $bot->del_handler('privcmd listfeed', 'RSSReader_listfeed');
+
+  $ua->_cleanup();
 
   $bot->del_help('addfeed', 'RSSReader');
   $bot->del_help('delfeed', 'RSSReader');
