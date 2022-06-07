1b3398f1d082d54bed1342dc498044cc4faf3215
Aaron <aaron@xanadu.ephasic.org>
2022-06-07 16:23:35 -0500

    added windows support for uptime

--- shadow.bat
+++ shadow.bat
@@ -2,7 +2,7 @@
 TITLE Shadow IRC Bot
 color 57
 
-echo "Starting Shadow IRC Bot by Aaron Blakely"
+echo Starting Shadow IRC Bot by Aaron Blakely
 echo.
 
 "C:\Program Files\Git\bin\bash.exe" -c "/c/Strawberry/perl/bin/perl.exe ./shadow -n -v"
