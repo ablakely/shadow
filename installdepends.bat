ae951c4d4b692d03ea69f1979bb9d7efe8c7e4ef
Aaron Blakely <aaron@ephasic.org>
2022-09-10 07:29:28 -0500

    updated installdepends.bat

--- installdepends.bat
+++ installdepends.bat
@@ -3,7 +3,25 @@ TITLE Shadow Dependency Installer
 color 57
 
 echo Starting Shadow IRC Bot Dependency Installer by Aaron Blakely
+echo --- DO NOT CLOSE UNTIL PROMPTED TO! ---
 echo.
 
-"C:\Program Files\Git\bin\bash.exe" -c "/c/Strawberry/perl/bin/perl.exe ./installdepends.pl"
-pause
+REM Sleep for 5 seconds 
+ping 127.0.0.0.1 -n 1 -w 5000
+
+IF %1.==. GOTO NOPATHEXISTS >nul
+GOTO PATHEXISTS
+
+
+:NOPATHEXISTS
+  "C:\Program Files\Git\bin\bash.exe" -c "/c/Strawberry/perl/bin/perl.exe ./installdepends.pl"
+   GOTO SAFEEXIT
+
+:PATHEXISTS
+    "C:\Program Files\Git\bin\bash.exe" -c "/c/Strawberry/perl/bin/perl.exe %1/installdepends.pl"
+    GOTO SAFEEXIT
+
+:SAFEEXIT
+  echo.
+  echo --- It is now safe to close this window! ---
+  pause
